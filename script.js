125f62dfdc72f7294f5baaf1d5929fddf5eb7165
261bayleyd <261bayley@gmail.com>
2025-06-05 18:12:10 +0100

    update!

---
+++ script.js
@@ -0,0 +1,186 @@
+let week1monday = {
+    mainMeal: "Lemon, Thyme & Garlic Infused Roast Chicken",
+    vegetarian: "Red Lentil Daal with Fresh Lemon & Parsley",
+    sideItems: "Cous Cous (Wheat) & Roasted Broccoli",
+    dessert: "Fruit Salad & Vanilla Yoghurt (Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week1tuesday = {
+    mainMeal: "Lamb Bolognese",
+    vegetarian: "Chickpea Ragu",
+    sideItems: "Penne Pasta (Wheat) & Garden Peas",
+    dessert: "Fruit Salad & Yoghurt (Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week1wednesday = {
+    mainMeal: "Build Your Own Chicken Tortilla (Wheat)",
+    vegetarian: "Home Made Falafel (Sesame FREE)",
+    sideItems: "Guacamole, Salsa, Hummus (Sesame FREE) & Lime Rice",
+    dessert: "Raspberry Mousse Sponge (Wheat, Egg & Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week1thursday = {
+    mainMeal: "Beef Loaded Mac & Cheese (Wheat & Milk)",
+    vegetarian: "Asparagus Gnocchi (Wheat & Milk)",
+    sideItems: "Penne Pasta (Wheat) & Sweetcorn",
+    dessert: "Fruit Salad & Yoghurt (Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week1friday = {
+    mainMeal: "Cod Loin with Fresh Tomato & Basil (Fish)",
+    vegetarian: "Margherita Pizza (Wheat & Milk) (Dairy & Gluten Free Available)",
+    sideItems: "Mixed Sweet & Maris Piper Skinny Wedges",
+    dessert: "Chocolate Sponge Cake (Wheat, Egg & Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week2monday = {
+    mainMeal: "Classic Beef Lasagne (Wheat & Milk)",
+    vegetarian: "Lentil & Vegetable Bolognese",
+    sideItems: "Fusilli Pasta (Wheat) & Green Beans",
+    dessert: "Fruit Salad & Yoghurt (Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week2tuesday = {
+    mainMeal: "Roast Chicken with Onion Gravy",
+    vegetarian: "Spring Vegetable Bake (Wheat & Milk)",
+    sideItems: "Crushed Potatoes & Baked Carrots",
+    dessert: "Fruit Salad & Yoghurt (Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week2wednesday = {
+    mainMeal: "Beef Tacos (Wheat)",
+    vegetarian: "Black Bean & Carrot Tacos",
+    sideItems: "Steamed Rice, Guacamole & Salsa",
+    dessert: "Peach Sponge with Whipped Cream (Wheat, Egg & Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week2thursday = {
+    mainMeal: "Spanish Marinated Chicken Breast",
+    vegetarian: "Potato Frittata (Egg)",
+    sideItems: "Handmade Roasted Potato Wedges & Garden Peas",
+    dessert: "Fruit Salad & Yoghurt (Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week2friday = {
+    mainMeal: "Salmon Fish Cake (Wheat & Fish)",
+    vegetarian: "Cheese & Tomato Pasta Bake (Wheat & Milk) topped with fresh Basil",
+    sideItems: "Penne Pasta (Wheat), Steamed Broccoli & Kale",
+    dessert: "Apple Crumble & Custard (Wheat, Egg & Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week3monday = {
+    mainMeal: "Beef Ragu",
+    vegetarian: "Tuscan Style Lentil & Roasted Courgette Ragu",
+    sideItems: "Spaghetti (Wheat), Roasted Cauliflower",
+    dessert: "Melon Slices with Yogurt",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week3tuesday = {
+    mainMeal: "Chicken Gyros (Milk)",
+    vegetarian: "Spanakopita (Wheat, Egg & Milk)",
+    sideItems: "Baked Chips, Sweetcorn & Pitta Bread (Wheat)",
+    dessert: "Mixed Berry Compote & Plain Yogurt (Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week3wednesday = {
+    mainMeal: "Lamb Stew",
+    vegetarian: "Pasta Funghi with Lions Mane (Wheat & Milk)",
+    sideItems: "Penne Pasta & Garden Peas",
+    dessert: "Blueberry Sponge (Wheat, Egg & Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week3thursday = {
+    mainMeal: "Chicken Tenders (Wheat) with Buffalo Sauce (Milk)",
+    vegetarian: "Cauliflower Cheese Gnocchi (Wheat & Milk)",
+    sideItems: "Roasted New Potatoes & Sweetcorn",
+    dessert: "Summer Fruits & Rice Pudding (Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+  
+  let week3friday = {
+    mainMeal: "Baked Salmon with Herbs & Garlic (Fish)",
+    vegetarian: "Summer Mac & Cheese (Wheat & Milk)",
+    sideItems: "Sweet Potato Wedges & Baked Beans",
+    dessert: "Coconut & Jam Sponge Cake (Wheat, Egg & Milk)",
+    alsoAvailable: "French Bread (Wheat), Jacket Potato Station (Fillings Contain Milk), Salad Bar, Fruit & Yoghurt (contains Milk)"
+  };
+let nolunch = "Sorry, We don't have lunch data for this date"
+let allfood = [
+    week1monday,week1tuesday,week1wednesday,week1thursday,week1friday,nolunch,nolunch,week2monday,week2tuesday,week2wednesday,week2thursday,week2friday,nolunch,nolunch,week3monday,week3tuesday,week3wednesday,week3thursday,week3friday,nolunch,nolunch
+]
+let valid
+function Validate (userinput) {
+    let result = document.getElementById("result")
+    if (userinput > new Date ("06/26/2025")){
+        result.innerHTML = "Sorry, We don't have lunch data for this date"
+        valid = false
+    }
+    else if (userinput < new Date ("04/21/2025")){
+        result.innerHTML = "Sorry, We don't have lunch data for this date"
+        valid = false
+    }
+    else{
+        valid = true
+    }
+}
+function go(){
+    // let daysToSubtract
+    let inputDate = document.getElementById("inputDate")
+    let day = new Date(inputDate.value)
+    Validate(day)
+    let result = document.getElementById("result")
+    // result.innerHTML = day.getDay()
+    const firstDate = new Date("04/21/2025") // 10th May, 2022
+    const secondDate = new Date(day) // today, 14th May, 2022
+    
+    const firstDateInMs = firstDate.getTime()
+    const secondDateInMs = secondDate.getTime()
+    
+    const differenceBtwDates = secondDateInMs - firstDateInMs
+    
+    const aDayInMs = 24 * 60 * 60 * 1000
+    
+    const datenum = Math.round(differenceBtwDates / aDayInMs)
+console.log(datenum)
+console.log(day)
+let main = document.getElementById("main")
+let veg = document.getElementById("veg")
+let sides = document.getElementById("sides")
+let dessert = document.getElementById("dessert")
+let also = document.getElementById("also")
+let fields = document.getElementById("fields")
+if (valid == true){
+    let goodthing = allfood[datenum%21]
+    if (goodthing == "Sorry, We don't have lunch data for this date"){
+        result.innerHTML = goodthing
+        fields.style.display = "none"   
+    }
+    else{
+        fields.style.display = "block"
+        main.innerHTML = goodthing.mainMeal
+        veg.innerHTML = goodthing.vegetarian
+        sides.innerHTML = goodthing.sideItems
+        dessert.innerHTML = goodthing.dessert
+        also.innerHTML = goodthing.alsoAvailable
+    }
+}
+else{
+        fields.style.display = "none"
+}
+    // day.getDay()
+    // const pastDate = new Date(day)
+    // pastDate.setDate(pastDate.getDate() - daysToSubtract)
+}
